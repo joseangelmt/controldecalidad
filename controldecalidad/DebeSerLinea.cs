08bf0c42099b3ad93d00827125c1495bb7478486
José Ángel Martínez Torres <joseangelmt@outlook.com>
2021-02-18 08:42:52 +0100

    Add project files.

---
+++ controldecalidad/DebeSerLinea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Digi21.DigiNG.DigiTab;
+using Digi21.DigiNG.Entities;
+
+namespace controldecalidad
+{
+    public class DebeSerLinea : ITrigger
+    {
+        public Entity AnalyzeEntity(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes, bool addingEntity)
+        {
+            if (entity is ReadOnlyLine)
+                return entity;
+
+            throw new AggregateException(new GeometryException("Esta geometría debería ser de tipo línea"));
+        }
+    }
+}
---
+++ controldecalidad/DebeTenerNumeroParDePuntos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Digi21.DigiNG.DigiTab;
+using Digi21.DigiNG.Entities;
+
+namespace controldecalidad
+{
+    public class DebeTenerNumeroParDePuntos : ITrigger
+    {
+        public Entity AnalyzeEntity(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes, bool addingEntity)
+        {
+            if (!(entity is ReadOnlyLine))
+                return entity;
+
+            var linea = entity as ReadOnlyLine;
+
+            if (linea.Points.Count % 2 != 0)
+                throw new AggregateException(new GeometryException("Solo se admiten líneas con un número par de puntos"));
+
+            return entity;
+        }
+    }
+}
